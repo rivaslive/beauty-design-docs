e4b6c01b6a0ba00407c905aef3e8e30520459f82
krlopez3 <krivas@brandy.la>
2021-09-28 18:26:32 -0600

    fixed tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -102,18 +102,20 @@ class MyDocument extends Document {
 
 					{/* Google Tag Manager */}
 					<script
-						async
-						src={`https://www.googletagmanager.com/gtag/js?id=GTM-W96QDMD`}
+						dangerouslySetInnerHTML={{
+							__html: `
+								dataLayer = [];
+							`,
+						}}
 					/>
 					<script
 						dangerouslySetInnerHTML={{
 							__html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'GTM-W96QDMD', {
-              page_path: window.location.pathname,
-            });
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W96QDMD');
           `,
 						}}
 					/>
